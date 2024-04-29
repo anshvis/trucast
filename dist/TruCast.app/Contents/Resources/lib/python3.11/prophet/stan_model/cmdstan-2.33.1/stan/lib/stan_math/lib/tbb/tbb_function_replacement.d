ba1521146d458380c0a63f62c33c7e2019731bcc
Ansh Viswanathan <ansh.viswanathan@gmail.com>
2024-04-28 21:31:17 -0400

    fixed everything

--- dist/TruCast.app/Contents/Resources/lib/python3.11/prophet/stan_model/cmdstan-2.33.1/stan/lib/stan_math/lib/tbb/tbb_function_replacement.d
+++
@@ -1,3 +0,0 @@
-tbb_function_replacement.o: \
-  ../tbb_2020.3/src/tbbmalloc/tbb_function_replacement.cpp \
-  ../tbb_2020.3/include/tbb/tbb_config.h
--- dist/TruCast.app/Contents/Resources/lib/python3.11/prophet/stan_model/cmdstan-2.33.1/stan/lib/stan_math/lib/tbb/x86_rtm_rw_mutex.d
+++
@@ -1,2 +0,0 @@
-x86_rtm_rw_mutex.o: ../tbb_2020.3/src/tbb/x86_rtm_rw_mutex.cpp \
-  ../tbb_2020.3/include/tbb/tbb_config.h
